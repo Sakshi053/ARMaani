eebf679211b92f62798490f8321b8a5aa8229b89
AvatarCogNiX <sakshichoudhary396@gmail.com>
2023-08-02 13:10:28 +0530

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=Delhi">Flats</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Mumbai">Hotels</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Bengaluru">PGs</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Hyderabad">GuestHouse</a>
+            </div>
+        </div>
+        <div class="footer-copyright">© 2023 Copyright ARMaani </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
